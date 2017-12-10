6ec431da1e7bd472d7eda0fd0ef7db79ed96eff9
Ilia Kuchmin <kuchmin@haulmont.com>
2017-12-10 21:41:40 +0400

    Initial commit

---
+++ src/main/java/com/bortsov/internetshop/InternetShopApplication.java
@@ -0,0 +1,12 @@
+package com.bortsov.internetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetShopApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bortsov/internetshop/InternetShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bortsov.internetshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InternetShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
